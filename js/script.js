2a0fbca22b67f32515a7b3d5680c588c10b4bbc8
GraficaPT <graficapt.comercial@gmail.com>
2025-08-21 13:35:38 +0100

    3

--- js/script.js
+++ js/script.js
@@ -3,8 +3,8 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 // --------------------------------------------------
 // Configurações Supabase & Google Apps Script
 // --------------------------------------------------
-const SUPABASE_URL  = 'https://neylqqdzdjxinzrhatjx.supabase.co'
-const SUPABASE_ANON = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5leWxxcWR6ZGp4aW56cmhhdGp4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTIyNTI1MTQsImV4cCI6MjA2NzgyODUxNH0.hNTtAIDmVgM1WuFlQ2594RNYrlf8gECPERI-12XUmTc'
+const SUPABASE_URL  = 'https://nbcmqkcztuogflejswau.supabase.co'
+const SUPABASE_ANON = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5iY21xa2N6dHVvZ2ZsZWpzd2F1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk2ODY4OTMsImV4cCI6MjA2NTI2Mjg5M30.Rf4yHxgo_bh56tXwHn3jJgJQr0tOCbXc2rQU1R26tv8'
 const GSCRIPT_URL   = 'https://script.google.com/macros/s/AKfycbwPUUtQZyrM7M17c5elXLDyqw1ZY_GznlmXH0BbUwruJBxxfSRh-apUqpQx0CA_x-m5/exec'
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON)
